036de69b3b2b7323e935e46cf9168d56b1eb609a
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-08-23 01:50:42 +0200

    updated class NPC

--- src/main/java/model/NPC.java
+++ src/main/java/model/NPC.java
@@ -2,8 +2,21 @@ package model;
 
 public class NPC extends Occupant{
 
+    private int power;
+    private int health;
 
-    public NPC(String name, boolean isSolid){
+
+    public NPC(String name, boolean isSolid,  int power, int health){
         super(name, isSolid);
+        this.power = power;
+        this.health = health;
     }
+
+    public int getPower() { return power; }
+
+    public void setPower(int power) { this.power = power; }
+
+    public int getHealth() { return health; }
+
+    public void setHealth(int health) { this.health = health; }
 }
