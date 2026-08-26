ec8e2ce180056d7868aebd4ae75599fc60d7e6f2
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-08-26 23:54:05 +0200

    implementing stage and scenes

--- src/main/java/it/unicam/cs/mpgc/rpg125928/view/GameView.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125928/view/GameView.java
@@ -1,12 +1,34 @@
 package it.unicam.cs.mpgc.rpg125928.view;
 
-import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class GameView extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+public class GameView {
+
+    private final Stage primaryStage;
+
+
+    public GameView(Stage primaryStage){
+        this.primaryStage = primaryStage;
+    }
+
+    public void showMainMenu(){
+        Label titleLabel = new Label();
+        titleLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+
+        Button newGameButton = new Button("Nuova Partita");
+        //to do button to load an already started game
+        Button exitButton = new Button("Exit");
+
+        newGameButton.setOnAction(e -> showGameView());
+        exitButton.setOnAction(e -> primaryStage.close());
+
+    }
+
+
+    public void showGameView(){
 
     }
 }
