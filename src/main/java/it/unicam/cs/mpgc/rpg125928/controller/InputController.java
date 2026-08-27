c7c70be8b7ee0dcdef83ffee60e790996c5f4d87
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-08-28 01:26:31 +0200

    implemented InputController to capture keys from keyboard

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125928/controller/InputController.java
@@ -0,0 +1,32 @@
+package it.unicam.cs.mpgc.rpg125928.controller;
+
+
+import it.unicam.cs.mpgc.rpg125928.model.Direction;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
+public class InputController {
+
+    private final GameController gameController;
+
+    public  InputController(GameController gameController) {
+        this.gameController = gameController;
+    }
+
+    public void setUpListeners(Scene scene) {
+        scene.setOnKeyPressed(e -> {
+            KeyCode keyCode = e.getCode();
+
+            switch (keyCode) {
+                case UP, W -> gameController.onDirectionChange(Direction.UP);
+                case DOWN, S -> gameController.onDirectionChange(Direction.DOWN);
+                case LEFT, A -> gameController.onDirectionChange(Direction.LEFT);
+                case RIGHT, D -> gameController.onDirectionChange(Direction.RIGHT);
+
+                default -> {
+
+                }
+            }
+        });
+    }
+}
