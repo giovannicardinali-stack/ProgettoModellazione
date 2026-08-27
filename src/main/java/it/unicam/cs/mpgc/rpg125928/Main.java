fb9b00cf1a3b8e51487bb0981928443d676ab310
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-08-27 17:11:49 +0200

    started implementing gameplay graphic interface

--- src/main/java/it/unicam/cs/mpgc/rpg125928/Main.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125928/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
         view.showMainMenu();
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 }
--- src/main/java/it/unicam/cs/mpgc/rpg125928/view/GameView.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125928/view/GameView.java
@@ -1,7 +1,13 @@
 package it.unicam.cs.mpgc.rpg125928.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -15,7 +21,7 @@ public class GameView {
     }
 
     public void showMainMenu(){
-        Label titleLabel = new Label();
+        Label titleLabel = new Label("Menù Principale");
         titleLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
 
         Button newGameButton = new Button("Nuova Partita");
@@ -25,10 +31,29 @@ public class GameView {
         newGameButton.setOnAction(e -> showGameView());
         exitButton.setOnAction(e -> primaryStage.close());
 
+        VBox root = new VBox(15);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(titleLabel,newGameButton,exitButton);
+
+        Scene scene = new Scene(root,400,300);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 
-
     public void showGameView(){
+
+        BorderPane gameRoot = new BorderPane();
+
+        //center zone: map
+        Pane mapArea = new Pane();
+        mapArea.setStyle("-fx-background-color: #1e1e1e;");
+
+        //downBar
+        VBox downBar = new VBox(5);
+        downBar.setPadding(new Insets(10));
+        downBar.setStyle("-fx-background-color: #222222;");
 
     }
 }
