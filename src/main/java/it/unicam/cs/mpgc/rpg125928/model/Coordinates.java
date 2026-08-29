c55cc9dc6015db928d08755e2e15b9dd5d1cd38d
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-08-29 23:07:59 +0200

    implemented equals() and hashcode() method in class Coordinates.java

--- src/main/java/it/unicam/cs/mpgc/rpg125928/model/Coordinates.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125928/model/Coordinates.java
@@ -17,4 +17,18 @@ public class Coordinates {
     public void setX(int x) { this.x = x; }
 
     public void setY(int y) { this.y = y; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x &&
+                y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(x, y);
+    }
 }
