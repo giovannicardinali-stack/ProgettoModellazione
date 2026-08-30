a604d6e71b7e2b604c9eea143c5d14987df3d577
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-08-30 19:53:13 +0200

    refactored MapGenerator, splitting the generateMap() in smaller private method

--- src/main/java/it/unicam/cs/mpgc/rpg125928/model/MapGenerator.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125928/model/MapGenerator.java
@@ -2,32 +2,40 @@ package it.unicam.cs.mpgc.rpg125928.model;
 
 public class MapGenerator {
 
+    private static final int MAP_SIZE = 15;
+
     public GameBoard generateMap(){
-        GameBoard gameBoard = new GameBoard(15);
+        GameBoard gameBoard = new GameBoard(MAP_SIZE);
 
-        //add perimeter walls
-        for (int i = 0; i < 15; i++) {
-            gameBoard.addOccupant(new Coordinates(i, 0), new Obstacle("Wall", true));
-            gameBoard.addOccupant(new Coordinates(i, 14), new Obstacle("Wall", true));
-            gameBoard.addOccupant(new Coordinates(0, i), new Obstacle("Wall", true));
-            gameBoard.addOccupant(new Coordinates(14, i), new Obstacle("Wall", true));
+        generatePerimeterWalls(gameBoard);
+        generateInternalWalls(gameBoard);
+        generateEntities(gameBoard);
+
+        return gameBoard;
+    }
+
+    private void generateInternalWalls(GameBoard gameBoard){
+        for (int y = 1; y <= 5; y++) {
+            gameBoard.addOccupant(new Coordinates(8, y), new Obstacle("Wall", true));
+            
         }
+    }
 
-        //add internal walls
-        gameBoard.addOccupant(new Coordinates(8, 1), new Obstacle("Wall", true));
-        gameBoard.addOccupant(new Coordinates(8, 2), new Obstacle("Wall", true));
-        gameBoard.addOccupant(new Coordinates(8, 3), new Obstacle("Wall", true));
-        gameBoard.addOccupant(new Coordinates(8, 4), new Obstacle("Wall", true));
-        gameBoard.addOccupant(new Coordinates(8, 5), new Obstacle("Wall", true));
+    private void generatePerimeterWalls(GameBoard gameBoard) {
+        int maxIndex = MAP_SIZE - 1;
+        for (int i = 0; i < MAP_SIZE; i++) {
+            gameBoard.addOccupant(new Coordinates(i, 0), new Obstacle("Wall", true));
+            gameBoard.addOccupant(new Coordinates(i, maxIndex), new Obstacle("Wall", true));
+            gameBoard.addOccupant(new Coordinates(0, i), new Obstacle("Wall", true));
+            gameBoard.addOccupant(new Coordinates(maxIndex, i), new Obstacle("Wall", true));
+        }
+    }
 
-
-
+    private void generateEntities(GameBoard gameBoard) {
         Player player = new Player("player1", true, 10, 10, 4);
         gameBoard.addOccupant(new Coordinates(3, 11), player);
 
         NPC npc = new NPC("enemy1", true, 8, 5, true, "");
         gameBoard.addOccupant(new Coordinates(11, 3), npc);
-
-        return gameBoard;
     }
 }
